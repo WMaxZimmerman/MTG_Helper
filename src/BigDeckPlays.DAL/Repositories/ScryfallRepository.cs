416d82dad0abf2d1323c12eced13e146b01865d1
Max Zimmerman <WMaxZimmerman@users.noreply.github.com>
2019-10-14 20:11:55 -0500

    Removed GetAllCards as it won't be used going forward (#6)
    * Removed GetAllCards as it won't be used going forward
    
    * Updated GetCards to be function based

--- src/BigDeckPlays.DAL/Repositories/ScryfallRepository.cs
+++ src/BigDeckPlays.DAL/Repositories/ScryfallRepository.cs
@@ -1,75 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using BigDeckPlays.Shared.Models;
-using Newtonsoft.Json;
-
 namespace BigDeckPlays.DAL.Repositories
 {
     public interface IScryfallRepository
     {
-        IEnumerable<Card> GetCards();
+        string GetFuzzyCardUrl(string card);
     }
     
-    public class ScryfallRepository
+    public class ScryfallRepository : IScryfallRepository
     {
-        private IConsoleRepository _console;
+        private IApiRepository _api;
+        public const string HostUrl = "https://api.scryfall.com/";
         
-        public ScryfallRepository(IConsoleRepository console = null)
+        public ScryfallRepository()
         {
-            _console = console ?? new ConsoleRepository();
-        }
-        
-        public IEnumerable<Card> GetCards(IEnumerable<string> cardNames)
-        {
-            var baseurl = "https://api.scryfall.com/cards/named?fuzzy=";
-
-            foreach(var name in cardNames)
-            {
-                var parsedName = name.Replace(' ', '+');
-                var card = GetStuff<Card>(baseurl + parsedName);
-                _console.WriteLine(card.Name + " (" + card.Cmc + ")");
-            }
-
-            return null;
         }
 
-        public IEnumerable<Card> GetCards()
+        public string GetFuzzyCardUrl(string card)
         {
-            // API Ends at 1477
-            var baseurl = "https://api.scryfall.com/cards?page=1";
+            var baseurl = HostUrl + "cards/named?fuzzy=";
+            var parsedName = card.Replace(' ', '+');
 
-            while(baseurl != null)
-            {
-                var data = GetStuff<ReturnObj>(baseurl);
-                baseurl = data?.Next_Page;
-
-                foreach(var card in data.Data.Distinct())
-                {
-                    _console.WriteLine(card.Name);
-                }
-            }
-
-            return null;
+            return baseurl + parsedName;
         }
-
-        private T GetStuff<T>(string url)
-        {
-            var client = new HttpClient();
-            var res = client.GetAsync(url).Result;
-            var content = res.Content;
-
-            var rawdata = content.ReadAsStringAsync().Result;
-            var data = JsonConvert.DeserializeObject<T>(rawdata);
-
-            return data;
-        }
-    }
-
-    public class ReturnObj
-    {
-        public List<Card> Data { get; set; }
-        public string Next_Page { get; set; }
     }
 }
--- src/BigDeckPlays.Tests/DAL/Repositories/TestApiRepository.cs
+++ src/BigDeckPlays.Tests/DAL/Repositories/TestApiRepository.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using BigDeckPlays.Shared.Models;
-using Newtonsoft.Json;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BigDeckPlays.DAL.Repositories;
 
 namespace BigDeckPlays.Tests.DAL.Repositories
 {
     [TestClass]
-    public class TestScryfallRepository
+    public class TestApiRepository
     {
-        private ScryfallRepository _repo;
+        private ApiRepository _repo;
 
         [TestInitialize]
         public void Init()
         {
+            _repo = new ApiRepository();
         }
     }
 }
