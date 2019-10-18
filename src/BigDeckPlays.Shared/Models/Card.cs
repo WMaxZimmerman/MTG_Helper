64ac84f36987c0fa17609bd97f9b85d8fc8e42c7
WMaxZimmerman <william.m.zimmerman@gmail.com>
2019-10-18 17:51:56 -0500

    Adds things to stream through files and start focusing more on inventory

--- src/BigDeckPlays.Shared/Models/Card.cs
+++ src/BigDeckPlays.Shared/Models/Card.cs
@@ -5,6 +5,7 @@ namespace BigDeckPlays.Shared.Models
     public class Card:  IEquatable<Card>
     {
         public string Name {get;set;}
+        public int Quantity {get;set;}
         public double Cmc {get;set;}
 
         public bool Equals(Card other)
